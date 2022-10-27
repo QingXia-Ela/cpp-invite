e61bac4b95077d25fbe001c6efff3bbff1a8d138
QingXia-Ela <Elahere@163.com>
2022-10-27 11:01:02 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/up2017-particle-effect/',
+  base: '/',
   server: {
     host: '0.0.0.0',
     port: 5173
